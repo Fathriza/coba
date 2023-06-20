b3a731f193a6ecf74220fc5ae617e211af4b6f8d
fathiza <fathriza563@gmail.com>
2023-06-20 20:02:52 +0700

    bismillah

--- src/ProjectEcoBites/Controller/LandingPageController.java
+++ src/ProjectEcoBites/Controller/LandingPageController.java
@@ -1,26 +1,35 @@
 package ProjectEcoBites.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import ProjectEcoBites.OpenScene;
+
    public class LandingPageController implements Initializable {
    
-    @FXML
-    private Button logButton;
-    @FXML
-    private Button signButton;
      @FXML
     private BorderPane border;
-    
-    
+
+    @FXML
+    private ImageView logButton;
+
+    @FXML
+    private Button signButton;
+
+    @FXML
+    void keLogin(ActionEvent event) {
+
+    }
     
     
     @Override
