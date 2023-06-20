94d939a0836cd99c08d562e63e3451bb69eab42a
fathiza <fathriza563@gmail.com>
2023-06-20 17:16:24 +0700

    coba

---
+++ src/ProjectEcoBites/Controller/LandingPageController.java
@@ -0,0 +1,38 @@
+package ProjectEcoBites.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+   public class LandingPageController implements Initializable {
+   
+    @FXML
+    private Button logButton;
+    @FXML
+    private Button signButton;
+     @FXML
+    private BorderPane border;
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+
+
+
+    public ImageView getImageView() {
+        return null;
+    }    
+    
+}
---
+++ src/ProjectEcoBites/OpenScene.java
@@ -0,0 +1,37 @@
+package ProjectEcoBites;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.net.URL;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author andhika
+ */
+public class OpenScene {
+    private Pane halaman;
+    
+    public Pane getPane(String fileName){
+        try{
+            URL fileHalaman=Main.class.getResource("/ProjectEcoBites/View"+fileName+".fxml");
+            
+            if(fileHalaman==null){
+                throw new java.io.FileNotFoundException("Halaman tidak ditemukan");
+            }
+            
+            halaman=new FXMLLoader().load(fileHalaman);
+            
+        }catch (Exception e){
+       
+            System.out.println("Tidak ditemukan halaman tersebut");
+        }
+        
+        return halaman;
+    }
+}
