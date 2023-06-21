60c8c6466f8b24d723c2c71a51e8f0f4d81b6462
Gatotbaik <22523254@students.uii.ac.id>
2023-06-21 14:24:06 +0700

    Tambah Chat Produsen

---
+++ src/ProjectEcoBites/Controller/userChatProdusenController.java
@@ -0,0 +1,34 @@
+package ProjectEcoBites.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import ProjectEcoBites.OpenScene;
+
+public class userChatProdusenController implements Initializable {
+   
+    @FXML
+    private BorderPane mainPane;
+    
+    @FXML
+    private void keTampilanUtamaP(ActionEvent event) {
+        OpenScene object=new OpenScene();
+        Pane halaman=object.getPane("TampilanUtamaP");
+        mainPane.setCenter(halaman);
+    }
+
+   @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+}
